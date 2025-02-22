0f8389681f8e2c52887841471f0cfc32bfe5a063
Cristian Franco <cpercivatif@gmail.com>
2025-02-22 13:19:51 -0300

    agregado pydantic

--- app/src/api.py
+++ app/src/api.py
@@ -6,22 +6,32 @@ from pydantic import BaseModel
 import os
 import pandas as pd
 import numpy as np
+from pydantic import BaseModel, Field
 
-#class train_response(BaseModel):
-#    message: DataFrame
+#Clases de validación de Pydantic
+class TrainRequest(BaseModel):
+    model_name: str
+    episodes: int = Field(..., gt=0)  # Mayor a 0
+    days: int = Field(..., ge=1, le=365)  # Entre 1 y 365
+class TrainResponse(BaseModel):
+    progress: int  # Porcentaje de progreso
+    message: str
+class ModelsResponse(BaseModel):
+    models: list[str]
+class DataResponse(BaseModel):
+    data: list[str]
+
 
 app = FastAPI()
 
-@app.post("/train")
-async def train(request: Request):
-    data = await request.json()
-    print(data)
-    #resultado = train_model()
-    return StreamingResponse(train_model(data['model_name'], data['episodes'], data['days']), media_type="application/json")
+@app.post("/train", response_model=dict)
+async def train(request: TrainRequest):
+    return StreamingResponse(
+        train_model(request.model_name, request.episodes, request.days), 
+        media_type="application/json")
 
-
-@app.get("/output")
-async def output(model_name):
+@app.get("/output", response_class=Response)
+async def output(model_name: str):
     file_path = f"serving\\outputs\\{model_name}_output.csv"
     with open(file_path, "r", encoding="utf-8") as f:
         csv_content = f.read()
@@ -31,13 +41,13 @@ async def output(model_name):
 async def read_root():
     return {"message": "Hello World"}
 
-@app.get("/models")
+@app.get("/models", response_model=ModelsResponse)
 async def read_models():
     models_path = "serving\\models"
     models = os.listdir(models_path)
     return {"models": models}
 
-@app.get("/get-data")
+@app.get("/get-data", response_model=DataResponse)
 async def read_models():
     data_path = "serving\\app\\src\\data"
     #Listar solo los que terminen en csv
